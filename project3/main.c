b986114a2f307ef7436cdc002728250ead30623f
kingken40 <offdawall316@gmail.com>
2021-07-26 18:35:41 -0600

    updating

---
+++ project3/main.c
@@ -0,0 +1,16 @@
+//Alternate LEDs from Off, Green, and Red
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+#include "switches.h"
+#include "buzzer.h"
+int main(void) {
+  configureClocks();/* setup master oscillator, CPU & peripheral clocks */
+  led_init();
+  buzzer_init();
+  buzzer_set_period(0);
+  switch_init();
+  enableWDTInterrupts();/* enable periodic interrupt */
+
+  or_sr(0x18);/* CPU off, GIE on */
+}
---
+++ project3/stateMachines.h
@@ -0,0 +1,11 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void state_advance();
+void red_toggle_on();
+void red_toggle_off();
+void green_toggle_on();
+void green_toggle_off();
+
+
+#endif // included
